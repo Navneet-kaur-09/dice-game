65fcb2e10f2c37e5ebe0d388428b6f8f452c43b6
Navneet-kaur-09 <87312769+Navneet-kaur-09@users.noreply.github.com>
2024-07-02 00:28:44 +0200

    Update index.js

--- DICE_GAME/index.js
+++ DICE_GAME/index.js
@@ -51,3 +51,13 @@ else {
     document.querySelector(".img2").setAttribute("src", "./images/dice6.png")
 
 }
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!!";
+} 
+else {
+    document.querySelector("h1").innerHTML = "Oops that's a Draw!!";
+}
